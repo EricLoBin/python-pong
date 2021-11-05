c43878fb2acdd3750e8ba338b84bf08948fc2263
EricLoBin <ekilobin@gmail.com>
2021-11-04 22:50:22 -0300

    [ADD] Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Import
+import threading
+
+# Import Files
+from src.render import render
+
+
+def main():
+    render_thread = threading.Thread(target=render)
+    render_thread.start()
+    print("oi")
+
+if (__name__ == "__main__"):
+    main()
---
+++ src/render.py
@@ -0,0 +1,8 @@
+import time
+from graphics import *
+
+def render():
+    
+    print("Thread: starting")
+    time.sleep(2)
+    print("Thread: finishing")
