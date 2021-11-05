81b251b1df4e007adf4a9c8ac39084edeef700cc
EricLoBin <ekilobin@gmail.com>
2021-11-05 12:32:29 -0300

    [UPDATE] Main menu added, getting mouseY

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 # Import
+from graphics import *
 import threading
 
 # Import Files
-from src.render import render
+from src.render import startMenu, render
 
+# Window
+width, height = 800, 600
 
+# Player
+playerY = height/2
+playerYGoal = height/2
+
+# Main
 def main():
-    render_thread = threading.Thread(target=render)
-    render_thread.start()
-    print("\noi")
+    # Create window
+    window = GraphWin("python-pong", width, height)
+
+    # Start Menu
+    startMenu(window, width, height)
+    
+
+    # Get mouse Y
+    def motion(event):
+        playerYGoal = event.y
+        point = Point(300, playerYGoal)
+        point.setFill("Red")
+        point.draw(window)
+    window.bind('<Motion>', motion)
+
+    # render_thread = threading.Thread(target=render)
+    # render_thread.start()
+    
+    render(window)
+    print(window.mouseY)
+    
+    window.getMouse()
+    window.close()
 
 if (__name__ == "__main__"):
     main()
--- src/render.py
+++ src/render.py
@@ -1,8 +1,20 @@
-import time
 from graphics import *
 
-def render():
+def startMenu(window, x, y):
+    window.setBackground("#444444")
+
+    rectangle = Rectangle(Point(10, 10), Point(x-10, y-10))
+    rectangle.setFill("#222222")
+    rectangle.draw(window)
     
-    print("Thread: starting")
-    time.sleep(2)
-    print("Thread: finishing")
+    text = Text(Point(x/2, y/2), "Play")
+    text.setSize(30)
+    text.draw(window)
+    
+    window.getMouse()
+    
+    rectangle.undraw()
+    text.undraw()
+
+def render(window):
+    window.setBackground("#444444")
