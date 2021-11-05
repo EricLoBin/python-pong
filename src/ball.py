416eb01a6aba9e13df47bc77668403577e1eb651
EricLoBin <ekilobin@gmail.com>
2021-11-05 20:27:49 -0300

    [UPDATE] moveBall function

--- src/ball.py
+++ src/ball.py
@@ -1,6 +1,16 @@
+import math
+
 def reflectBall(angle):
     newAngle = 0
     if (angle < 180):
         return (180 - angle)
     else:
         return (540 - angle)
+
+
+def moveBall(x, y, angle, step):
+    radians = 0.01745329 * (angle - 90) # PI/180 * angle
+    return [
+        x + step*math.cos(radians),
+        y + step*math.sin(radians)
+    ]
