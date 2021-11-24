429a62719997d9b1d366084ec8ffe86a4a1fc3b7
EricLoBin <ekilobin@gmail.com>
2021-11-24 13:48:53 -0300

    [UPDATE] faster key input

--- src/player.py
+++ src/player.py
@@ -1,12 +1,14 @@
 def movePlayer(gameData):
     newY = (gameData["player"]["y"] + gameData["player"]["stepSize"] if (gameData["player"]["yGoal"] > gameData["player"]["y"]) else gameData["player"]["y"] - gameData["player"]["stepSize"]) if ((gameData["player"]["yGoal"] - gameData["player"]["y"]) > gameData["player"]["stepSize"] or (gameData["player"]["y"] - gameData["player"]["yGoal"]) > gameData["player"]["stepSize"]) else gameData["player"]["y"]
 
-    gameData["player"]["element"].move(gameData["player"]["x"] - gameData["player"]["x"], newY - gameData["player"]["y"])
     key = gameData["window"]["element"].checkKey()
-
     if key == "Up" and gameData["player"]["yGoal"] > 0:
-        gameData["player"]["yGoal"] -= gameData["player"]["stepSize"]
+        newY -= gameData["player"]["stepSize"]
+        gameData["player"]["yGoal"] = gameData["player"]["y"]
     elif key == "Down" and gameData["player"]["yGoal"] < gameData["window"]["height"]:
-        gameData["player"]["yGoal"] += gameData["player"]["stepSize"]
+        newY += gameData["player"]["stepSize"]
+        gameData["player"]["yGoal"] = gameData["player"]["y"]
     
+    gameData["player"]["element"].move(gameData["player"]["x"] - gameData["player"]["x"], newY - gameData["player"]["y"])
+
     return newY
